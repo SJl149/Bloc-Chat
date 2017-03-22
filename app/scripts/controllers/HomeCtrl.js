3a6d2c5c87513e219c3692ac2eef1edc30dec80d
Scott Lenander <sjlteacher@gmail.com>
2017-03-21 20:49:48 -0400

    Added some style for current chatroom

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,15 @@
 (function () {
     function HomeCtrl(Room, $uibModal) {
         this.chatRooms = Room.all;
+        /* this.currentRoom = {};
+        Change currentRoom function? 
+        
+        this.displayCurrentRoom = function(roomName) {
+            this.currentRoom = roomName;
+            
+        }
+        
+        */
         
         this.open = function() {
            var modalInstance = $uibModal.open({
